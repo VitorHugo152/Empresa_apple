80a98dbd17befe4060ef1c05272a81ce39304d8c
VitorHugo152 <vitorhugo1800@hotmail.com>
2022-03-15 00:52:38 -0300

    Update Projeto1 apple.sql

---
+++ databaseFiles/Projeto1 apple.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Iphone 2G (
+    codigo NUMBER (100),
+    nome VARCHAR (11),
+    marca VARCHAR (20),
+    cor VARCHAR (60)
+);
+
+CREATE TABLE Iphone 2G (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+);
+
+CREATE TABLE Funcionario (
+nome VARCHAR (100),
+idade NUMBER (3),
+nacionalidade VARCHAR (100),
+idioma VARCHAR (100)
+);
+
+CREATE TABLE Cliente (
+nome VARCHAR (100),
+idade NUMBER (3),
+nacionalidade VARCHAR (100),
+idioma VARCHAR (100)
+);
+
+CREATE TABLE Lojas_Fisicas (
+nome
+
+);
