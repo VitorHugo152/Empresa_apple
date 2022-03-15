afb4c643b56fe6f4bdcb69ddeaaf4096ea1fc4cc
VitorHugo152 <vitorhugo1800@hotmail.com>
2022-03-15 18:35:12 -0300

    Update Projeto1 apple.sql

--- databaseFiles/Projeto1 apple.sql
+++ databaseFiles/Projeto1 apple.sql
@@ -2,14 +2,98 @@ CREATE TABLE Iphone 2G (
     codigo NUMBER (100),
     nome VARCHAR (11),
     marca VARCHAR (20),
-    cor VARCHAR (60)
+    cor VARCHAR (60),
+    ano DATE
 );
 
-CREATE TABLE Iphone 2G (
+CREATE TABLE Iphone 3G (
 codigo NUMBER (100),
 nome VARCHAR (11),
 marca VARCHAR (20),
 cor VARCHAR (60),
+ano DATE
+);
+
+CREATE TABLE Iphone 3GS (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+CREATE TABLE Iphone 4 (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+CREATE TABLE Iphone 5 (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+CREATE TABLE Iphone 5C (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+CREATE TABLE Iphone 5S (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+CREATE TABLE Iphone 6 (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+CREATE TABLE Iphone 6 Plus (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+CREATE TABLE Iphone 6S  (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+
+CREATE TABLE Iphone 6S Plus (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
+);
+
+
+CREATE TABLE Iphone SE (
+codigo NUMBER (100),
+nome VARCHAR (11),
+marca VARCHAR (20),
+cor VARCHAR (60),
+ano DATE
 );
 
 CREATE TABLE Funcionario (
