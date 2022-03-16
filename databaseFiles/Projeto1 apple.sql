70e2b99d99c8c4286b254612238f668cea1c48b1
VitorHugo152 <vitorhugo1800@hotmail.com>
2022-03-16 06:51:08 -0300

    Update Projeto1 apple.sql

--- databaseFiles/Projeto1 apple.sql
+++ databaseFiles/Projeto1 apple.sql
@@ -101,6 +101,7 @@ nome VARCHAR (100),
 idade NUMBER (3),
 nacionalidade VARCHAR (100),
 idioma VARCHAR (100)
+salario
 );
 
 CREATE TABLE Cliente (
@@ -114,3 +115,43 @@ CREATE TABLE Lojas_Fisicas (
 nome
 
 );
+
+INSERT INTO Iphone 2G (codigo,nome,marca,cor,ano)
+VALUES (123,'Iphone','apple','preto','2007-06-29' )
+
+INSERT INTO Iphone 3G (codigo,nome,marca,cor,ano)
+VALUES (133,'Iphone','apple','vermelho','2008-06-09' )
+
+INSERT INTO Iphone 3GS (codigo,nome,marca,cor,ano)
+VALUES (144,'Iphone','apple','azul','2009-12-21' )
+
+INSERT INTO Iphone 4 (codigo,nome,marca,cor,ano)
+VALUES (145,'Iphone','apple','amarelo','2010-06-24' )
+
+INSERT INTO Iphone 5 (codigo,nome,marca,cor,ano)
+VALUES (155,'Iphone','apple','roxo','2012-09-21' )
+
+INSERT INTO Iphone 5C (codigo,nome,marca,cor,ano)
+VALUES (166,'Iphone','apple','verde','2013-09-20' )
+
+INSERT INTO Iphone 5S (codigo,nome,marca,cor,ano)
+VALUES (177,'Iphone','apple','Azul escuro','2013-09-20' )
+
+INSERT INTO Iphone 6 (codigo,nome,marca,cor,ano)
+VALUES (188,'Iphone','apple','verde claro','2014-11-14' )
+
+INSERT INTO Iphone 6 Plus (codigo,nome,marca,cor,ano)
+VALUES (199,'Iphone','apple','Rosa','2014-09-09' )
+
+INSERT INTO Iphone 6s (codigo,nome,marca,cor,ano)
+VALUES (200,'Iphone','apple','cinza','2015-09-09' )
+
+INSERT INTO Iphone 6s Plus (codigo,nome,marca,cor,ano)
+VALUES (400,'Iphone','apple','laranja','2015-09-25' )
+
+INSERT INTO Iphone SE (codigo,nome,marca,cor,ano)
+VALUES (400,'Iphone','apple','laranja','2016-03-18' )
+
+
+
+SELECT * FROM Iphone 2G
