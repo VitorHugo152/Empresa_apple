8ae4b99c0a7c4e36c3e13e4d29e558967070e1c8
VitorHugo152 <vitorhugo1800@hotmail.com>
2022-07-27 18:04:41 -0300

    Update Empresa apple.sql

--- Empresa apple.sql
+++ Empresa apple.sql
@@ -1,75 +1,84 @@
-CREATE TABLE Iphones(
-codigo NUMBER (10) PRIMARY KEY;
+CREATE TABLE Iphones (
+codigo NUMERIC (30) PRIMARY KEY,
 nome VARCHAR (30),
 modelo VARCHAR (30),
-ano_de_lançamento DATE,
-cor VARCHAR (30)
+ano DATE ,
+cor VARCHAR (26),
+preco NUMERIC (10,2)
+)
 
-);
-
-CREATE TABLE IPad(
-codigo NUMBER (10) PRIMARY KEY;
+CREATE TABLE IMACS (
+codigo NUMERIC (30) PRIMARY KEY,
 nome VARCHAR (30),
 modelo VARCHAR (30),
-ano_de_lançamento DATE,
-cor VARCHAR (30)
+ano DATE ,
+cor VARCHAR (26)
+)
 
-);
 
-CREATE TABLE Macbooks(
-codigo NUMBER (10) PRIMARY KEY;
+CREATE TABLE MACBOOKS (
+codigo NUMERIC (30) PRIMARY KEY,
 nome VARCHAR (30),
 modelo VARCHAR (30),
-ano_de_lançamento DATE,
-cor VARCHAR (30)
+ano DATE ,
+cor VARCHAR (26)
+)
 
-);
-
-CREATE TABLE IMACS(
-codigo NUMBER (10) PRIMARY KEY;
-nome VARCHAR (30),
-modelo VARCHAR (30),
-ano_de_lançamento DATE,
-cor VARCHAR (30)
-
-);
 
 CREATE TABLE Funcionario (
-nome VARCHAR (100),
-idade NUMBER (3),
-nacionalidade VARCHAR (100),
-idioma VARCHAR (100),
-salario NUMBER (10,2)
-);
+RG NUMERIC  (30) PRIMARY KEY,
+nome VARCHAR(30),
+salario NUMERIC (10,2)
+)
 
-CREATE TABLE Cliente (
-nome VARCHAR (100),
-idade NUMBER (3),
-nacionalidade VARCHAR (100),
-idioma VARCHAR (100)
-);
+CREATE TABLE Clientes (
+RG NUMERIC  (30) PRIMARY KEY,
+nome VARCHAR (30),
+conta VARCHAR (20),
+email VARCHAR (28),
+telefone NUMERIC (15)
+)
 
 CREATE TABLE Lojas_Fisicas (
-nome
-endereco
-);
+endereco VARCHAR (33) PRIMARY KEY,
+nome VARCHAR (33)
+)
+
+CREATE TABLE Lojas_Digitais (
+nome VARCHAR (33),
+URL VARCHAR (33) PRIMARY KEY,
+suporte VARCHAR (33)
+
+)
 
 INSERT INTO Iphones (codigo,nome,modelo,ano_de_lançamento,cor)
-VALUES (123,'Iphone ','apple','2007-06-29','preto' )
+VALUES (123,'Iphone ','A1203','2007-06-29','preto' )
 
 
 INSERT INTO Iphones (codigo,nome,modelo,ano_de_lançamento,cor)
-VALUES (123,'Iphone 3G','apple','2008-06-29','preto' )
+VALUES (321,'Iphone 3G',' A1324','2008-06-09','branco' )
 
 
 INSERT INTO Iphones (codigo,nome,modelo,ano_de_lançamento,cor)
-VALUES (123,'Iphone 3GS','apple','2007-06-29','preto' )
+VALUES (231,'Iphone 3GS','A1325','2009-06-08','vermelho' )
 
 
 INSERT INTO Iphones (codigo,nome,modelo,ano_de_lançamento,cor)
-VALUES (123,'Iphone 4','apple','2007-06-29','preto' )
+VALUES (400,'Iphone 4',' A1349','2010-06-24','amarelo' )
+
+INSERT INTO IMACS (codigo,nome,modelo,ano_de_lançamento,cor)
+VALUES (1011,'Macintosh 128k',' 128k','1984-01-24','branco' )
+
+INSERT INTO IMACS (codigo,nome,modelo,ano_de_lançamento,cor)
+VALUES (1102,'Macintosh 512k',' 512k','1984-09-10','branco' )
+
+INSERT INTO MACBOOKS (codigo,nome,modelo,ano_de_lançamento,cor)
+VALUES (1105,'Macbook Pro',' 512k','2006-01-10','verde' )
+
+INSERT INTO MACBOOKS (codigo,nome,modelo,ano_de_lançamento,cor)
+VALUES (1106,'Macintosh 512k',' 512k','1984-09-10','preto' )
 
 
+SELECT * FROM Iphones
 
 
-SELECT * FROM Iphone
